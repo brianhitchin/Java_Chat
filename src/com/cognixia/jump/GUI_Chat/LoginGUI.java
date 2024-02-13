a54601671def76db2437f58c554233a63e803a2b
Wendy Navarro <xxwendy3xx@github.com>
2024-02-13 12:21:34 -0500

    gui templates done

--- src/com/cognixia/jump/GUI_Chat/LoginGUI.java
+++ src/com/cognixia/jump/GUI_Chat/LoginGUI.java
@@ -1,79 +1,66 @@
 package com.cognixia.jump.GUI_Chat;
+
 import java.io.IOException;
 import java.net.Socket;
+
 import javafx.application.Application;
-import com.cognixia.jump.client.Client;
-
-
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-
 import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
-
 import javafx.stage.Stage;
 
-
 public class LoginGUI extends Application {
-	
-    private TextField messageField;
-    private TextField  usernameField;
-    private TextField  passwordField;
-    private Client client;
+
+    private TextField usernameField;
+    private TextField passwordField;
 
     @Override
     public void start(Stage primaryStage) {
         BorderPane root = new BorderPane();
-        
-      ///********************LOGIN IN********************************************************** 
-      
- 
-        try {
-            Socket socket = new Socket("0.0.0.0", 1234); 
-            client = new Client(socket, "username"); 
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        VBox LoginArea= new VBox();
-        LoginArea.setPadding(new Insets(10));
-        LoginArea.setSpacing(10);
+
+        VBox loginArea = new VBox();
+        loginArea.setPadding(new Insets(10));
+        loginArea.setSpacing(10);
 
         usernameField = new TextField();
         usernameField.setPromptText("Username");
-        
+
         passwordField = new TextField();
         passwordField.setPromptText("Password");
-    
-        
-        LoginArea.getChildren().addAll(usernameField, passwordField);
-        root.setCenter(LoginArea);
-       
-        Button loginButton= new Button("Login");
-       // loginButton.setOnAction(event -> login());
-        root.setBottom(loginButton);
-    
-        
-      
-        
-        
-        Scene Loginscene = new Scene(root, 600, 400);
-        primaryStage.setScene(Loginscene);
-        primaryStage.setTitle("LOGIN ");
-       
-        primaryStage.show();
-        }
- 
 
-    public static void main(String[] args) {
-       launch(args);
+        loginArea.getChildren().addAll(usernameField, passwordField);
+        root.setCenter(loginArea);
+
+        Button loginButton = new Button("Login");
+        loginButton.setOnAction(event -> login());
+        VBox buttonArea = new VBox();
+        buttonArea.getChildren().add(loginButton);
+        buttonArea.setAlignment(Pos.CENTER);
+        BorderPane.setMargin(buttonArea, new Insets(0,0,350,0)); // Top margin to separate from loginArea
+        root.setBottom(buttonArea);
+   
+
+        Scene loginScene = new Scene(root, 400, 150);
+        primaryStage.setScene(loginScene);
+        primaryStage.setTitle("LOGIN");
+        primaryStage.show();
     }
 
+    private void login() {
+        // Handle login logic here
+        String username = usernameField.getText();
+        String password = passwordField.getText();
+        // Perform authentication, socket connection, etc.
+    }
 
-	
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
-
 
 
 
